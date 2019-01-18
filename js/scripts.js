2c4a27f0cb17df080c572fa74db63625c3987c5d
Joe Pritchett <joempritchett@gmail.com>
2019-01-18 13:50:09 -0800

    combine the four separate answer tracking functions into one mega function that is able to track the amount of all answers(a, b, c ,d) singlehandedly. its is probably poorly optimized but it works. also verified that it is able to display a hidden element corresponding to whichever answer had the majority votes.

--- js/scripts.js
+++ js/scripts.js
@@ -48,15 +48,12 @@ $(document).ready(function()  {
     var q5a4 = parseInt($("input:radio[id=q5a4]:checked").val());
 
     // var a1Total = q1a1 + q2a1 + q3a1 + q4a1 + q5a1;
-    a1Total(q1a1, q2a1, q3a1, q4a1, q5a1)
-    a2Total(q1a2, q2a2, q3a2, q4a2, q5a2)
-    a3Total(q1a3, q2a3, q3a3, q4a3, q5a3)
-    a4Total(q1a4, q2a4, q3a4, q4a4, q5a4)
+    calcTotals(q1a1, q2a1, q3a1, q4a1, q5a1, q1a2, q2a2, q3a2, q4a2, q5a2, q1a3, q2a3, q3a3, q4a3, q5a3, q1a4, q2a4, q3a4, q4a4, q5a4)
   });
 
 });
 
-function a1Total(a, b, c, d, e)  {
+function calcTotals(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t)  {
   var a1Total = 0;
   if (a === 1)  {
     var a1Total = a1Total + 1;
@@ -83,95 +80,101 @@ function a1Total(a, b, c, d, e)  {
   } else {
     var a1Total = a1Total + 0;
   }
-  alert(a1Total);
-}
-
-function a2Total(a, b, c, d, e)  {
   var a2Total = 0;
-  if (a === 1)  {
+  if (f === 1)  {
     var a2Total = a2Total + 1;
   } else {
     var a2Total = a2Total + 0;
   }
-  if (b === 1)  {
+  if (g === 1)  {
     var a2Total = a2Total + 1;
   } else {
     var a2Total = a2Total + 0;
   }
-  if (c === 1)  {
+  if (h === 1)  {
     var a2Total = a2Total + 1;
   } else {
     var a2Total = a2Total + 0;
   }
-  if (d === 1)  {
+  if (i === 1)  {
     var a2Total = a2Total + 1;
   } else {
     var a2Total = a2Total + 0;
   }
-  if (e === 1)  {
+  if (j === 1)  {
     var a2Total = a2Total + 1;
   } else {
     var a2Total = a2Total + 0;
   }
-  alert(a2Total);
-}
-
-function a3Total(a, b, c, d, e)  {
   var a3Total = 0;
-  if (a === 1)  {
+  if (k === 1)  {
     var a3Total = a3Total + 1;
   } else {
     var a3Total = a3Total + 0;
   }
-  if (b === 1)  {
+  if (l === 1)  {
     var a3Total = a3Total + 1;
   } else {
     var a3Total = a3Total + 0;
   }
-  if (c === 1)  {
+  if (m === 1)  {
     var a3Total = a3Total + 1;
   } else {
     var a3Total = a3Total + 0;
   }
-  if (d === 1)  {
+  if (n === 1)  {
     var a3Total = a3Total + 1;
   } else {
     var a3Total = a3Total + 0;
   }
-  if (e === 1)  {
+  if (o === 1)  {
     var a3Total = a3Total + 1;
   } else {
     var a3Total = a3Total + 0;
   }
-  alert(a3Total);
+  var a4Total = 0;
+  if (p === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (q === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (r === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (s === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (t === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  tallyResults(a1Total, a2Total, a3Total ,a4Total)
 }
 
-function a4Total(a, b, c, d, e)  {
-  var a4Total = 0;
-  if (a === 1)  {
-    var a4Total = a4Total + 1;
-  } else {
-    var a4Total = a4Total + 0;
+function tallyResults(a1, a2, a3, a4) {
+  if (a1 > a2 && a3 && a4)  {
+    $(".aLang").show();
+  } else if (a2 > a1 && a3 && a4) {
+    $(".bLang").show();
+  } else if (a3 > a1 && a2 && a4) {
+    $(".cLang").show();
+  } else if (a4 > a1 && a2 && a3) {
+    $(".dLang").show();
+  } else tieBreaker()
+}
+
+function tieBreaker(a1, a2, a3, a4){
+  if (a1===a2)  {
+    $()
   }
-  if (b === 1)  {
-    var a4Total = a4Total + 1;
-  } else {
-    var a4Total = a4Total + 0;
-  }
-  if (c === 1)  {
-    var a4Total = a4Total + 1;
-  } else {
-    var a4Total = a4Total + 0;
-  }
-  if (d === 1)  {
-    var a4Total = a4Total + 1;
-  } else {
-    var a4Total = a4Total + 0;
-  }
-  if (e === 1)  {
-    var a4Total = a4Total + 1;
-  } else {
-    var a4Total = a4Total + 0;
-  }
-  alert(a4Total);
 }
