5f20ec24002c8e521ac2df764829b86351749a6c
Joe Pritchett <joempritchett@gmail.com>
2019-01-18 13:13:00 -0800

    apply code from previous commit to all form answers, also fix typo in said code. code can now track how many times user selected a, how many times user selected b, etc..

--- js/scripts.js
+++ js/scripts.js
@@ -27,28 +27,31 @@ $(document).ready(function()  {
   $("#multQuiz").submit(function(event) {
     event.preventDefault();
     var q1a1 = parseInt($("input:radio[id=q1a1]:checked").val());
-    var q1a2 = parseInt($("input:radio[name=q1a2]:checked").val());
-    var q1a3 = parseInt($("input:radio[name=q1a3]:checked").val());
-    var q1a4 = parseInt($("input:radio[name=q1a4]:checked").val());
+    var q1a2 = parseInt($("input:radio[id=q1a2]:checked").val());
+    var q1a3 = parseInt($("input:radio[id=q1a3]:checked").val());
+    var q1a4 = parseInt($("input:radio[id=q1a4]:checked").val());
     var q2a1 = parseInt($("input:radio[id=q2a1]:checked").val());
-    var q2a2 = parseInt($("input:radio[name=q2a2]:checked").val());
-    var q2a3 = parseInt($("input:radio[name=q2a3]:checked").val());
-    var q2a4 = parseInt($("input:radio[name=q2a4]:checked").val());
+    var q2a2 = parseInt($("input:radio[id=q2a2]:checked").val());
+    var q2a3 = parseInt($("input:radio[id=q2a3]:checked").val());
+    var q2a4 = parseInt($("input:radio[id=q2a4]:checked").val());
     var q3a1 = parseInt($("input:radio[id=q3a1]:checked").val());
-    var q3a2 = parseInt($("input:radio[name=q3a2]:checked").val());
-    var q3a3 = parseInt($("input:radio[name=q3a3]:checked").val());
-    var q3a4 = parseInt($("input:radio[name=q3a4]:checked").val());
+    var q3a2 = parseInt($("input:radio[id=q3a2]:checked").val());
+    var q3a3 = parseInt($("input:radio[id=q3a3]:checked").val());
+    var q3a4 = parseInt($("input:radio[id=q3a4]:checked").val());
     var q4a1 = parseInt($("input:radio[id=q4a1]:checked").val());
-    var q4a2 = parseInt($("input:radio[name=q4a2]:checked").val());
-    var q4a3 = parseInt($("input:radio[name=q4a3]:checked").val());
-    var q4a4 = parseInt($("input:radio[name=q4a4]:checked").val());
+    var q4a2 = parseInt($("input:radio[id=q4a2]:checked").val());
+    var q4a3 = parseInt($("input:radio[id=q4a3]:checked").val());
+    var q4a4 = parseInt($("input:radio[id=q4a4]:checked").val());
     var q5a1 = parseInt($("input:radio[id=q5a1]:checked").val());
-    var q5a2 = parseInt($("input:radio[name=q5a2]:checked").val());
-    var q5a3 = parseInt($("input:radio[name=q5a3]:checked").val());
-    var q5a4 = parseInt($("input:radio[name=q5a4]:checked").val());
+    var q5a2 = parseInt($("input:radio[id=q5a2]:checked").val());
+    var q5a3 = parseInt($("input:radio[id=q5a3]:checked").val());
+    var q5a4 = parseInt($("input:radio[id=q5a4]:checked").val());
 
     // var a1Total = q1a1 + q2a1 + q3a1 + q4a1 + q5a1;
     a1Total(q1a1, q2a1, q3a1, q4a1, q5a1)
+    a2Total(q1a2, q2a2, q3a2, q4a2, q5a2)
+    a3Total(q1a3, q2a3, q3a3, q4a3, q5a3)
+    a4Total(q1a4, q2a4, q3a4, q4a4, q5a4)
   });
 
 });
@@ -70,15 +73,105 @@ function a1Total(a, b, c, d, e)  {
   } else {
     var a1Total = a1Total + 0;
   }
-  if (c === 1)  {
-    var a1Total = a1Total + 1;
-  } else {
-    var a1Total = a1Total + 0;
-  }
   if (d === 1)  {
     var a1Total = a1Total + 1;
   } else {
     var a1Total = a1Total + 0;
   }
+  if (e === 1)  {
+    var a1Total = a1Total + 1;
+  } else {
+    var a1Total = a1Total + 0;
+  }
   alert(a1Total);
+}
+
+function a2Total(a, b, c, d, e)  {
+  var a2Total = 0;
+  if (a === 1)  {
+    var a2Total = a2Total + 1;
+  } else {
+    var a2Total = a2Total + 0;
+  }
+  if (b === 1)  {
+    var a2Total = a2Total + 1;
+  } else {
+    var a2Total = a2Total + 0;
+  }
+  if (c === 1)  {
+    var a2Total = a2Total + 1;
+  } else {
+    var a2Total = a2Total + 0;
+  }
+  if (d === 1)  {
+    var a2Total = a2Total + 1;
+  } else {
+    var a2Total = a2Total + 0;
+  }
+  if (e === 1)  {
+    var a2Total = a2Total + 1;
+  } else {
+    var a2Total = a2Total + 0;
+  }
+  alert(a2Total);
+}
+
+function a3Total(a, b, c, d, e)  {
+  var a3Total = 0;
+  if (a === 1)  {
+    var a3Total = a3Total + 1;
+  } else {
+    var a3Total = a3Total + 0;
+  }
+  if (b === 1)  {
+    var a3Total = a3Total + 1;
+  } else {
+    var a3Total = a3Total + 0;
+  }
+  if (c === 1)  {
+    var a3Total = a3Total + 1;
+  } else {
+    var a3Total = a3Total + 0;
+  }
+  if (d === 1)  {
+    var a3Total = a3Total + 1;
+  } else {
+    var a3Total = a3Total + 0;
+  }
+  if (e === 1)  {
+    var a3Total = a3Total + 1;
+  } else {
+    var a3Total = a3Total + 0;
+  }
+  alert(a3Total);
+}
+
+function a4Total(a, b, c, d, e)  {
+  var a4Total = 0;
+  if (a === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (b === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (c === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (d === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  if (e === 1)  {
+    var a4Total = a4Total + 1;
+  } else {
+    var a4Total = a4Total + 0;
+  }
+  alert(a4Total);
 }
